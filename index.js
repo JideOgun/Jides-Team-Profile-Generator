06833601d5b100157b3814b8911f39d35abc8df3
JideOgun <JideOgun@github.com>
2022-01-16 20:02:08 -0600

    added page-template.js and generate-site.js for html

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
 
 
 Employee.prototype.initializeEmployee = function() {
